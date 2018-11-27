f04b34fc04f291ee03af3325e7af8281ec8449a0
Cauim de S. Lima <cauimsouza@gmail.com>
2018-11-27 21:43:29 +0100

    Implement initializePriorityQueue

--- src/ContinuousDijkstra.java
+++ src/ContinuousDijkstra.java
@@ -21,6 +21,29 @@ public class ContinuousDijkstra {
 		}
 	}
 
+	private void initializePriorityQueue(
+			Vertex<Point_3> source,
+			PriorityQueue<Window> pq
+	) {
+		Halfedge<Point_3> firstHalfedge = source.getHalfedge();
+		Halfedge<Point_3> halfedge = firstHalfedge;
+		do {
+			double lengthHalfedge = calculateLengthHalfedge(halfedge);
+			Window fullEdgeWindow =
+				new Window(0, lengthHalfedge, lengthHalfedge, 0, 0, halfedge, 0);
+			pq.add(fullEdgeWindow);
+
+			halfedge = halfedge.getNext().getOpposite();
+		} while (halfedge != firstHalfedge);
+	}
+
+	private double calculateLengthHalfedge(Halfedge<Point_3> halfedge) {
+		Vertex<Point_3> destination = halfedge.getVertex();
+		Vertex<Point_3> origin = halfedge.getOpposite().getVertex();
+
+		return (double) destination.getPoint().distanceFrom(origin.getPoint());
+	}
+
 	public double calculateDistance(Vertex<Point_3> destination) {
 		return 0;
 	}
