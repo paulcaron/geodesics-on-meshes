002fd3611189b6850725a57fb8531ff3085ae963
Cauim de S. Lima <cauimsouza@gmail.com>
2018-11-27 21:17:11 +0100

    Implement method buildDistances

--- src/ContinuousDijkstra.java
+++ src/ContinuousDijkstra.java
@@ -11,6 +11,14 @@ public class ContinuousDijkstra {
 
 	public void buildDistances(Vertex<Point_3> source) {
 		PriorityQueue<Window> pq = new PriorityQueue<>();
+
+		initializePriorityQueue(source, pq);
+
+		while (!pq.empty()) {
+			Window window = pq.poll();
+
+			propagateWindow(window, pq);
+		}
 	}
 
 	public double calculateDistance(Vertex<Point_3> destination) {
